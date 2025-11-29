3090801229cc7343e8fd985308cc6c975b18d6b5
deepakdashcode108 <deepakdashcode108@gmail.com>
2025-11-30 01:03:42 +0530

    Update route.ts

--- src/app/api/home/route.ts
+++ src/app/api/home/route.ts
@@ -1,6 +1,6 @@
 export async function GET(request: Request) {
   const users = [
-    { id: 1, name: 'Alice' },
+    { id: 1, name: 'Alice in Borderland' },
     { id: 2, name: 'Bobby' }
   ];
   return Response.json(users);
